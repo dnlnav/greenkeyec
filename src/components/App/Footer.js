61e432c4e1caf4ce7b6a5a7bdfeac000aaebbf1a
Daniel Navarrete <cdng9202@gmail.com>
2020-11-22 15:24:08 -0500

    Update footer

--- src/components/App/Footer.js
+++ src/components/App/Footer.js
@@ -100,17 +100,6 @@ const Footer = () => {
                 </a>
               </p>
             </div>
-
-            <div className="col-lg-6 col-md-6">
-              <ul>
-                <li>
-                  <Link to="/privacy-policy">Privacy Policy</Link>
-                </li>
-                <li>
-                  <Link to="/terms-of-service">Terms & Conditions</Link>
-                </li>
-              </ul>
-            </div>
           </div>
         </div>
       </div>
